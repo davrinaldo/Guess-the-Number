b928d23363f708a55b90de8eb236600b732c6abb
David Rinaldo <david.felipe.rinaldo@hotmail.com>
2024-05-29 22:52:23 -0300

    fix bug message

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# guess-the-number
-#Guess the Number
+# Guess The Number
+
+## Descrição
+Este é um jogo simples de "Adivinhe o Número" desenvolvido em Python. O objetivo do jogo é adivinhar um número secreto gerado pelo computador dentro de um intervalo específico, com base na dificuldade escolhida pelo usuário.
+
+## Como Jogar
+1. Clone o repositório para o seu ambiente local.
+2. Navegue até o diretório do projeto.
+3. Execute o script Python `guess_the_number.py`.
+4. Siga as instruções exibidas no console para escolher a dificuldade e fazer suas tentativas para adivinhar o número secreto.
+
+## Exemplo de Uso
--- src/game_logic.py
+++ src/game_logic.py
@@ -1,3 +1,28 @@
+"""
+MIT License
+
+Copyright (c) [2024] [David Felipe Rinaldo]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
 import random
 
 
@@ -12,24 +37,30 @@ Digite 1, 2 ou 3 para continuar:"""
 \033[1;31mDigite apenas 1, 2 ou 3 para selecionar uma Dificuldade!\033[m"""
 
     MESSAGE_NUMBER_ERROR = """
-\033[1;31mDIGITE APENAS NÚMEROS!\033[m"""
+\033[1;31mDIGITE APENAS NÚMEROS ENTRE 1 e"""
 
     def __init__(self):
+        """Inicializa a classe GuessTheNumber."""
+
         self.max_num = 3
         self.max_num = self.get_user_number(self.MESSAGE_DIFFICULTY)
         self.secret_number = self.generate_number_secret()
         self.attempts = 0
 
     def get_user_number(self, prompt):
+        """Solicita ao usuário um número de acordo com o prompt fornecido."""
+
         while True:
             try:
                 number = int(input(prompt))
                 if self.validate_user_number(number):
                     return number
             except ValueError:
-                print(self.MESSAGE_NUMBER_ERROR)
+                print(self.MESSAGE_NUMBER_ERROR, f'{self.max_num}!\033[m')
 
     def validate_user_number(self, number):
+        """Valida se o número fornecido pelo usuário está dentro dos limites"""
+
         if 1 <= number <= self.max_num:
             return True
         else:
@@ -37,6 +68,7 @@ Digite 1, 2 ou 3 para continuar:"""
             return False
 
     def generate_number_secret(self):
+        """Gera um número secreto dentro do intervalo de dificuldade."""
         return random.randint(1, self.max_num)
 
 
